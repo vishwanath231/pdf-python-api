25ff2fe2b57a6f92b411e692786ba41d9d326425
Vishwanath <vishwanathvishwabai@gmail.com>
2024-07-03 00:17:53 +0530

    final

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ def extract_tables():
     return jsonify(json_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
